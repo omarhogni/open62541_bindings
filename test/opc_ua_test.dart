f204d38a6e82fe40b20d0ffe3dac6d1ab5701002
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-06-14 13:15:51 +0000

    Cleanup

--- test/opc_ua_test.dart
+++ test/opc_ua_test.dart
@@ -1,16 +1,14 @@
-import 'package:opc_ua/opc_ua.dart';
+//TODO: Add some tests
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(true, true);
     });
   });
 }
